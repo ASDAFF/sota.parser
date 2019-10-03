5905491ccaca788aad17d09267dae20d3368d404
ASDAFF <asdaff.asad@yandex.ru>
2019-10-03 18:17:27 +0500

    update source

---
+++ source/shs.parser_5.5.4/install/_version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "5.1.10",
+	"VERSION_DATE" => "2015-07-01 12:18:41"
+);
+?>
---
+++ source/shs.parser_5.5.4/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "5.5.4",
+	"VERSION_DATE" => "2016-06-08 14:00:39"
+);
+?>
